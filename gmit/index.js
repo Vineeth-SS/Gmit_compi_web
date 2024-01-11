be9b13384f4dbaa75b50c7145a9bcc0d50896571
Vineeth-SS <137502907+Vineeth-SS@users.noreply.github.com>
2024-01-11 13:44:06 +0530

    Add files via upload

---
+++ gmit/index.js
@@ -0,0 +1,9 @@
+function showPopup() {
+  document.getElementById("popup-container").style.display = "block";
+  document.getElementById("popup-overlay").style.display = "block";
+}
+
+function hidePopup() {
+  document.getElementById("popup-container").style.display = "none";
+  document.getElementById("popup-overlay").style.display = "none";
+}
